b620e0e4a356def2d8c29b5d0d33a61689d7314b
Sourav Makur <souravmakur888@gmail.com>
2026-04-16 01:29:24 +0530

    BetterSol

--- Sliding Window and Two Pointers/LongestRepeatingCharacterReplacement.java
+++ Sliding Window and Two Pointers/LongestRepeatingCharacterReplacement.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class LongestRepeatingCharacterReplacement {
     public static int Brute(String s , int k) {
         int maxLen = 0;
@@ -19,9 +20,30 @@ public class LongestRepeatingCharacterReplacement {
         }
         return maxLen;
     }
+    public static int Better(String s , int k)  {
+        int maxLen = 0;
+        int n = s.length();
+        int r = 0;
+        int l = 0;
+        int maxFreq = 0;
+        int hash [] = new int [26];
+
+        while(r<n) {
+            hash[s.charAt(r) - 'A']++;
+            maxFreq = Math.max(maxFreq , hash[s.charAt(r) - 'A']);
+            while((r-l+1) - maxFreq > k ) {
+                hash[s.charAt(l) - 'A']--;
+                l++;
+            }
+            maxLen = Math.max(maxLen , r-l+1);
+            r++;
+        }
+        return maxLen;
+    }
     public static void main (String args[]) {
-        String s = "AABABBA";
+        String s = "AAABBCCD";
         int k = 2;
         System.out.println(Brute(s, k));
+        System.out.println(Better(s, k));
     }
 }
