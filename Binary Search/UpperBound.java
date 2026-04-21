59d548f75a4bf89dcc2f7412a57ba4c136020e26
Sourav Makur <souravmakur888@gmail.com>
2026-04-21 09:23:55 +0530

    UpperBound-LowerBound

---
+++ Binary Search/UpperBound.java
@@ -0,0 +1,24 @@
+public class UpperBound {
+    public static int iterative(int arr[] , int target) {
+        int ans = arr.length;
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high) {
+            int mid = (low+high)/2;
+            if(arr[mid] > target) {
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    
+    public static void main (String args[]) {
+        int arr[] = {2, 3, 7, 10, 11, 11, 25};
+        int target = 9;
+        System.out.println(iterative(arr, target));
+    }
+}
