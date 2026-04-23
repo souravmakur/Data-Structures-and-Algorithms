5e15332445237e62c610913ce94a482ed3ee6b71
Sourav Makur <souravmakur888@gmail.com>
2026-04-24 00:14:33 +0530

    search-in-rotated-sorted-array-ii

---
+++ Binary Search/SearchInRotatedSortedArrayII.java
@@ -0,0 +1,36 @@
+public class SearchInRotatedSortedArrayII {
+    public static boolean Solution(int arr[] , int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low<=high) {
+            int mid = (low + high) / 2;
+            if(arr[mid] == target) {
+                return true;
+            }
+            if(arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low ++;
+                high --;
+                continue;
+            }
+            if(arr[low] <= arr[mid]){
+                if(arr[low] <= target && target < arr[mid]) {
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(arr[mid] < target && target <= arr[high]) {
+                    low = mid - 1;
+                }else{
+                    high = mid + 1;
+                }
+            }
+        } 
+        return false;
+    }
+    public static void main (String args []) {
+        int arr[] = {3,0,2,3,3,3,3};
+        int target = 1;
+        System.out.println(Solution(arr, target));
+    }
+}
