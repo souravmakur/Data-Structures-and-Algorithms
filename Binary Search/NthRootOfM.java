3d3cbba1df2533c1c7b3af2c7ef0acc9896a903d
Sourav Makur <souravmakur888@gmail.com>
2026-05-14 02:35:07 +0530

    initial commit

--- Binary Search/NthRootOfM.java
+++ Binary Search/NthRootOfM.java
@@ -1,3 +1,4 @@
+import java.lang.management.OperatingSystemMXBean;
 import java.util.*;
 public class NthRootOfM {
     public static int helper (int i , int n) {
@@ -7,7 +8,7 @@ public class NthRootOfM {
         }
         return answer;
     }
-    public static boolean answer(int n , int m) {
+    public static boolean Brute(int n , int m) {
         int ans = 0;
         for(int i = 1; i <= n; i++) {
             if(helper(i , n) == m) {
@@ -16,9 +17,36 @@ public class NthRootOfM {
         }
         return false;
     }
+    public static int Optimal(int n , int m) {
+        if(m == 0) {
+            return 0;
+        }
+        if(m == 1) {
+            return 1;
+        }
+        int low = 1;
+        int high = n;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(helper(mid, n) == m) {
+                return mid;
+            }else if(helper(mid, n) > m) {
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
     public static void main (String args[]) {
-        int n = 4;
-        int m = 69;
-        System.out.println(answer(n , m));
+        int n = 3;
+        int m = 27;
+        int n1 = 4;
+        int m1= 69;
+        System.out.println(Brute(n , m));
+        System.out.println(Optimal(n , m));
+        System.out.println();
+        System.out.println(Brute(n1 , m1));
+        System.out.println(Optimal(n1 , m1));
     }
 }
